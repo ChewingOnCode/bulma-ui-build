07c035bbdb5b800439ec2dcccab5d8648ce4b95c
ChewingOnCode <karkinoscode@gmail.com>
2023-12-27 20:13:16 -0500

    updated Bulma UI project for tabs

--- index.js
+++ index.js
@@ -6,3 +6,24 @@ const navMenu = document.querySelector("#nav-links");
 burgerIcon.addEventListener("click", () => {
   navMenu.classList.toggle("is-active");
 });
+
+// Tabs
+const tabs = document.querySelectorAll(".tabs li");
+const tabContextBoxes = document.querySelectorAll("#tab-content > div");
+
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    tabs.forEach((item) => item.classList.remove("is-active"));
+    tab.classList.add("is-active");
+
+    const target = tab.dataset.target;
+    // console.log(target);
+    tabContextBoxes.forEach((box) => {
+      if (box.getAttribute("id") === target) {
+        box.classList.remove("is-hidden");
+      } else {
+        box.classList.add("is-hidden");
+      }
+    });
+  });
+});
