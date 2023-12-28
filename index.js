1c99375557274d0354e2bf94a2d34927d8089d31
ChewingOnCode <karkinoscode@gmail.com>
2023-12-28 04:21:49 -0500

    finished the modal

--- index.js
+++ index.js
@@ -27,3 +27,16 @@ tabs.forEach((tab) => {
     });
   });
 });
+
+// modal
+const signUpButton = document.querySelector("#signup");
+const modalBg = document.querySelector(".modal-background");
+const modal = document.querySelector(".modal");
+
+signUpButton.addEventListener("click", () => {
+  modal.classList.add("is-active");
+});
+
+modalBg.addEventListener("click", () => {
+  modal.classList.remove("is-active");
+});
